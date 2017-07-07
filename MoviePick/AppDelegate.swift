2c27926713a7a0e6379e249ef816f69a4ba6e6b3
Adithya Ravikumar <adithyaravikumar1988@gmail.com>
2017-07-07 10:54:56 -0400

    Updated the readme file

--- MoviePick/AppDelegate.swift
+++ MoviePick/AppDelegate.swift
@@ -38,7 +38,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
         let reachability = Reachability(hostname: GlobalConstants.OMDBServerURL)
         
         guard reachability != nil && (reachability?.isReachable)! else {
@@ -49,28 +48,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-        // Saves changes in the application's managed object context before the application terminates.
         saveContext()
     }
 
     // MARK: - Core Data stack
 
     lazy var applicationDocumentsDirectory: URL = {
-        // The directory the application uses to store the Core Data store file. This code uses a directory named "com.Adithya.MoviePick" in the application's documents Application Support directory.
         let urls = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
         return urls[urls.count-1] 
     }()
 
     lazy var managedObjectModel: NSManagedObjectModel = {
-        // The managed object model for the application. This property is not optional. It is a fatal error for the application not to be able to find and load its model.
         let modelURL = Bundle.main.url(forResource: "MoviePick", withExtension: "momd")!
         return NSManagedObjectModel(contentsOf: modelURL)!
     }()
 
     lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator? = {
-        // The persistent store coordinator for the application. This implementation creates and return a coordinator, having added the store for the application to it. This property is optional since there are legitimate error conditions that could cause the creation of the store to fail.
-        // Create the coordinator and store
         var coordinator: NSPersistentStoreCoordinator? = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
         let url = self.applicationDocumentsDirectory.appendingPathComponent("MoviePick.sqlite")
         var error: NSError? = nil
@@ -86,7 +79,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }()
 
     lazy var managedObjectContext: NSManagedObjectContext? = {
-        // Returns the managed object context for the application (which is already bound to the persistent store coordinator for the application.) This property is optional since there are legitimate error conditions that could cause the creation of the context to fail.
         let coordinator = self.persistentStoreCoordinator
         if coordinator == nil {
             return nil
@@ -96,6 +88,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return managedObjectContext
     }()
 
+    
+    
     // MARK: - Core Data Saving support
 
     func saveContext () {
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # MoviePick
-A simple movie search app using OMDB backend
+This is a simple movie search app. The app queries the Open Movie Database (OMDB) server for a search string and returns a single result.
+
+This project uses the following iOS APIs
+
+1) Core Data for caching
+2) NSURLSession for networking
+3) UIKit for displaying the UI
